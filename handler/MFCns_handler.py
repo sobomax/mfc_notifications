a77bc32c8b7088c0385274eafc9e92c534dd8b11
Li-Wen Hsu <lwhsu@lwhsu.org>
2020-04-24 16:52:40 +0800

    Mention the mfc service by gonzo@

--- handler/MFCns_handler.py
+++ handler/MFCns_handler.py
@@ -67,7 +67,8 @@ def sendnote(to, subject, branch, content):
          'believe that you received this message due to an error.',        \
          'P.P.S. Source code for this service is available at:',    \
          'https://github.com/sobomax/mfc_notifications',        \
-         'Have a feature in mind? Pull requests are always very welcome!',
+         'Have a feature in mind? Pull requests are always very welcome!',  \
+         'P.P.P.S. https://mfc.kernelnomicon.org is your friend!',
          '')]
     template.extend(content)
     template = [x.encode('utf-8') for x in template]
